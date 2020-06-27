641fb9f4d5ae7fd3b0e88c4abef122bb1fe74a47
teruto725 <akiteruto@gmail.com>
2020-06-27 16:59:52 +0900

    change a few

--- games/models.py
+++ games/models.py
@@ -4,13 +4,12 @@ from accounts.models import CustomUser
 GAME_OPTIONS = (("ja","じゃんけん"),("gomoku","五目並べ"))
 
 
-
-
-"""
 class Room(models.Model):
     question_text = models.CharField(max_length=200)
-    gameid = models.IntegerField(default=0)
-    user1 = models.ForeignKey(Agent,on_delete=models.CASCADE)
-"""
+    game = models.CharField(max_length=10)
+    user1 = models.ForeignKey(CustomUser,)
+    user1 = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+
+
 
 
